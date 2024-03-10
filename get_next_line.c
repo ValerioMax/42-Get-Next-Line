3009a8af468346416193029c052bd0cb79c9c685
valerio <valeriomax03@gmail.com>
2024-03-10 20:57:49 +0100

    tre

--- get_next_line.c
+++ get_next_line.c
@@ -1,48 +1,6 @@
 #include "get_next_line.h"
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-
-/*
-int	main(void)
-{
-	char	buf[BUFFER_SIZE];	// stores the characters read
-	char	b[100];
-	int	fd;		// file descriptor to read
-	int	nb_read;	// stores read's return value
-	int	count;		// counts the number of reads
-
-//	Open the cat.txt file in read only mode
-	fd = open("cat.txt", O_RDONLY);
-	if (fd == -1)
-		return (1);
-//	Initialize the count variables
-	nb_read = -1;
-	count = 0;
-	while (1)
-	{
-		nb_read = read(fd, buf, 1);
-		if (nb_read == -1)
-		{
-			printf("Read error!\n");
-			return (1);
-		}
-		b[count] = buf[0]; 
-		if (b[count] == '\n')
-		{
-			b[count] = '\0';
-			printf("%s\n", b);
-			break ;
-		}
-		//buf[nb_read] = '\0';
-		//printf("\e[36m%d : [\e[0m%s\e[36m]\e[0m\n", count, buf);
-		count++;
-	}
-//	Close the opened file descriptor
-	close(fd);
-	return (0);
-}
-*/
+#include <fcntl.h>	//provv
+#include <stdio.h>	//provv
 
 char *get_next_line(int fd)
 {
@@ -56,14 +14,17 @@ char *get_next_line(int fd)
 		return (0);
 	b = (char *) malloc(sizeof(char) * BUFFER_SIZE);
 	count = 0;
-	while (nb_read != -1 && buf[0] != '\n')
+	while (nb_read != -1 && count < BUFFER_SIZE)
 	{
 		nb_read = read(fd, buf, 1);
 		if (nb_read == -1)
 			return (NULL);
 		b[count] = buf[0]; 
-		if (b[count] == '\n')
-			b[count] = '\0';
+		if (b[count] == '\n' && count + 1 < BUFFER_SIZE)
+		{
+			b[count + 1] = '\0';
+			break ;
+		}
 		count++;
 	}
 	return (b);
@@ -75,5 +36,13 @@ int main()
 	char *s;
 
 	s = get_next_line(fd);
-	printf("%s\n", s);
+	if (s[10] == '\n')
+		printf("newline\n");
+	if (s[10] == '\0')
+		printf("terminating\n");
+	printf("1: %s\n", s);
+	//printf("1: %s\n", get_next_line(fd));
+	printf("2: %s\n", get_next_line(fd));
+	printf("3: %s\n", get_next_line(fd));
+	//printf("4: %s\n", get_next_line(fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: valerio <valerio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 18:02:01 by valerio           #+#    #+#             */
-/*   Updated: 2024/03/10 19:52:38 by valerio          ###   ########.fr       */
+/*   Updated: 2024/03/10 20:55:45 by valerio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
-//# include <stdio.h>
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 100
 # endif
+
+char	*get_next_line(int fd);
 
 #endif
