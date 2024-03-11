7b246e9979021aad07734fc32c7c132a3708e0f8
Valerio Massimo Dessena <vdessena@e4r11p2.42roma.it>
2024-03-11 19:27:57 +0100

    da 42

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: valerio <valerio@student.42.fr>            +#+  +:+       +#+        */
+/*   By: vdessena <vdessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 21:03:16 by valerio           #+#    #+#             */
-/*   Updated: 2024/03/10 21:03:25 by valerio          ###   ########.fr       */
+/*   Updated: 2024/03/11 19:23:51 by vdessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,46 +16,56 @@
 
 char *get_next_line(int fd)
 {
-	char	buf[1];
-	char	*b;
+	char	*buf;
+	static char	*b;
 	int		nb_read;
 	int		i;
 
 	if (fd == -1)
 		return (NULL);
-	b = (char *) malloc(sizeof(char) * BUFFER_SIZE);
-	if (!b);
+	b = (char *) malloc(sizeof(char) * (BUFFER_SIZE));
+	if (!b)
+		return (NULL);
+	buf = (char *) malloc(sizeof(char));
+	if (!buf)
 		return (NULL);
 	i = 0;
-	while (nb_read != -1 && i < BUFFER_SIZE)
+	while (i < BUFFER_SIZE)
 	{
 		nb_read = read(fd, buf, 1);
-		if (nb_read == -1)
+		if (nb_read <= 0)
+		{
+			free(buf);
+			free(b);
 			return (NULL);
-		b[i] = buf[0]; 
-		if (b[i] == '\n' && i + 1 < BUFFER_SIZE)
+		}
+		//	return (NULL);
+		b[i] = buf[0];
+		if (b[i] == '\n' && i  < BUFFER_SIZE) //i + 1?
 		{
 			b[i + 1] = '\0';
 			break ;
 		}
 		i++;
 	}
+	free(buf);
 	return (b);
 }
 
+
+
+
+
 int main()
 {
-	int fd = open("cat.txt", O_RDONLY);
-	char *s;
-
-	s = get_next_line(fd);
-	if (s[10] == '\n')
-		printf("newline\n");
-	if (s[10] == '\0')
-		printf("terminating\n");
-	printf("1: %s\n", s);
-	//printf("1: %s\n", get_next_line(fd));
-	printf("2: %s\n", get_next_line(fd));
-	printf("3: %s\n", get_next_line(fd));
-	//printf("4: %s\n", get_next_line(fd));
+	int fd = open("test/1-brouette.txt", O_RDONLY);
+	char *s = get_next_line(fd);
+	for (int i = 1; s != NULL; i++)
+	{
+		printf("%d: %s\n", i, s);
+		free(s);
+		s = get_next_line(fd);
+	}
+	return 0;
 }
+
